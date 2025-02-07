4c2525fdac201c28506e0684afbd191204c2eb55
Ksidles-Slyd <kyle.sidles@slyd.com>
2025-02-06 20:32:06 -0500

    Add Models.cs

---
+++ Models.cs
@@ -0,0 +1,9 @@
+namespace PhotonAPIClient.Models
+{
+    public class ExampleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    // Add more models based on LXDs REST API endpoints
+}
